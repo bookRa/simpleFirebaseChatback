bff3e50310a8ed79f700bf560ca5a717464feba2
unknown <omar.abdelbadie1@gmail.com>
2018-07-13 01:07:22 -0700

    that's good for now

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-var firebase = require("firebase");
-var firebaseui = require("firebaseui");
-
-let loginBtn = document.getElementById("loginBtn");
-let signupBtn = document.getElementById("signupBtn");
+var config = {
+  apiKey: "AIzaSyBCVy7QIFCrTOgnHrHPj2Zy8RtKRa4-NLA",
+  authDomain: "chatback-d00f9.firebaseapp.com",
+  databaseURL: "https://chatback-d00f9.firebaseio.com",
+  projectId: "chatback-d00f9",
+  storageBucket: "",
+  messagingSenderId: "165444001038"
+};
+firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(user => {
+  var name;
+  console.log("Hello World");
+  if (user) {
+    name = user.email;
+  } else {
+    name = "Anonymous Guest";
+  }
+  document.getElementById("userName").innerText = name;
+});
