19d4e9e665f6a5bee6b5c68e2d4c014d9c7041a2
unknown <omar.abdelbadie1@gmail.com>
2018-07-13 00:18:29 -0700

    testCommit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var firebase = require("firebase");
+var firebaseui = require("firebaseui");
+
 let loginBtn = document.getElementById("loginBtn");
 let signupBtn = document.getElementById("signupBtn");
-alert("hello!");
